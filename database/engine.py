bd70f0512a72ae4b231836a1f66d2bfdea2c07ad
Michael Bykovski <michael.bykovskii@aoe.com>
2020-01-12 12:13:03 +0100

    Remove completely check_same_thread

--- database/engine.py
+++ database/engine.py
@@ -3,7 +3,12 @@ import os
 from sqlalchemy import create_engine
 
 DB_URL = os.environ.get('DB_URL', 'sqlite:///./db.sqlite')
+connection_args = {}
+
+if 'sqlite' in DB_URL:
+    connection_args['check_same_thread'] = True
+
 engine = create_engine(
     DB_URL,
-    connect_args={"check_same_thread": 'sqlite' in DB_URL}
+    connect_args=connection_args,
 )
